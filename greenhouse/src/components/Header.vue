b5ec0c40d5d0a2ccd7565af899b796dc15744eab
lucas <lfpgc96@hotmail.com>
2019-05-01 03:54:45 -0300

    Edições no Vue e Prototipo de telas com bootstrap

--- greenhouse/src/components/Header.vue
+++ greenhouse/src/components/Header.vue
@@ -1,38 +1,9 @@
 <template> 
-    <div class="container">
-        <!--
-        <div class="row">
-            <NaviLogo/>
-            <Navibar/>
-        -->
-        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light"> -->
-        <!-- <nav class="navbar navbar-light" style="background-color: #00ff00;"> -->    
+    <div class="container">    
         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" style="background-color: #00ff00;">
             <NaviLogo/>
             <div class="ml-auto"></div>
             <div><Navibar/></div>
-                <!--
-                    <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                       <a class="nav-link" href="#">Sobre</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produtos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Serviços</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contato</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Sign in</a>
-                    </li>
-                </ul>
-                 -->
         </nav>
 
         </div>
