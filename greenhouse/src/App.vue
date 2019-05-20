bfe7dc587dd57ef7db5e10d1051e457f3e7f4d2d
lucas <lfpgc96@hotmail.com>
2019-05-20 15:55:26 -0300

    tabulação das paginas

--- greenhouse/src/App.vue
+++ greenhouse/src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <Navi/>
-    <Footer/>
-    
+    <nav class="navbar navbar-expand-lg navbar-light mr-auto">
+      <div class="collapse navbar-collapse ml-auto" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+          <li v-for="tab in tabs" :key="tab" @click="selected=tab" :class="['nav-item',{active: selected == tab}]">
+            <a class="nav-link">{{tab}}</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <component :is="selected" class="tab"></component>
   </div>
 </template>
 
 <script>
-import Navi from "./components/Navi.vue";
-import Footer from "./components/Footer.vue";
-
+import Inicio from "./components/Inicio.vue"
+import Sobre from "./components/Sobre.vue"
 export default {
   name: "app",
   components: {
-    Navi,
-    Footer
-  }
+    Inicio,  Sobre
+  },
+  data: () => {
+    return {
+      tabs: ["Inicio", "Sobre"],
+      selected: "Inicio"
+    };
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  
 }
 </style>
---
+++ greenhouse/src/components/Inicio.vue
@@ -0,0 +1,126 @@
+<template>
+  <main>
+    <div id="banner_home" style="background-color: #0EBE57; color: white;">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6" style="margin:auto;">
+            <div class="row">
+              <div class="col-sm-6">
+                <h1 style="text-align: right; ">
+                  <strong>Economize</strong>
+                </h1>
+              </div>
+              <div class="col-sm-6">
+                <h3 style="text-align: left;">dinheiro enquanto</h3>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6">
+                <h1 style="text-align: right;">
+                  <strong>Salva</strong>
+                </h1>
+              </div>
+              <div class="col-sm-6">
+                <h3 style="text-align: left;">o meio ambiente</h3>
+              </div>
+            </div>
+            <div class="row">
+              <br>
+              <br>
+              <p style="margin:auto;">
+                <strong>Se inscreva e faça parte da comunidade</strong>
+              </p>
+            </div>
+            <div class="row">
+              <button type="button" class="btn btn-dark" style="margin:auto;">Sign up</button>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <img src="assets/banner.png" height="428">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-7">
+          <br>
+          <br>
+          <h1>
+            <strong>Como funciona?</strong>
+          </h1>
+          <br>
+          <h3>
+            <strong>Se inscreva</strong>
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam
+            vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis,
+            consequat arcu. Pellentesque feugiat molestie enim a aliquam.
+          </p>
+        </div>
+        <div class="col-sm-5">
+          <img src="assets/browser.png" height="428">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-5">
+          <img src="assets/app.png" height="428">
+        </div>
+        <div class="col-sm-7">
+          <br>
+          <br>
+          <h3>
+            <strong>Peça o seu biodigestor</strong>
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam
+            vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis,
+            consequat arcu. Pellentesque feugiat molestie enim a aliquam.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-7">
+          <br>
+          <br>
+          <h3>
+            <strong>Comece à combater o desperdício de comida!</strong>
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vestibulum mauris ut diam
+            vulputate, nec scelerisque magna maximus. Suspendisse sit amet ex vestibulum, semper nunc quis,
+            consequat arcu. Pellentesque feugiat molestie enim a aliquam.
+          </p>
+        </div>
+        <div class="col-sm-5">
+          <img src="assets/trash.png" height="428">
+        </div>
+      </div>
+    </div>
+
+    <div id="banner_home" style="background-color: #0EBE57;color: white;">
+      <div class="container">
+        <div class="row" style="margin:auto;">
+          <div class="col-sm-6">
+            <h3>Pronto?</h3>
+            <h2>Comece agora mesmo!</h2>
+          </div>
+          <div class="col-sm-6" style="margin:auto;">
+            <button type="button" class="btn btn-dark">Sign up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name: "Inicio"
+};
+</script>
+
+<style scoped>
+</style>
