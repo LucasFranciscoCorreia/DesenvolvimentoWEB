6ac850ff81cd950e5520f849122e6f7085e12f54
Mayara Castro <maaycastro@hotmail.com>
2019-05-21 16:14:49 -0300

    pags produtos e serviços logado e não logado

--- greenhouse/src/App.vue
+++ greenhouse/src/App.vue
@@ -3,221 +3,272 @@
     <nav class="navbar navbar-expand-lg navbar-light mr-auto">
       <div class="collapse navbar-collapse ml-auto" id="navbarNav">
         <ul class="navbar-nav ml-auto">
-          <li
-            v-for="tab in tabs"
-            :key="tab"
-            @click="selected=tab"
-            :class="['nav-item',{active: selected == tab}]"
-          >
-            <a :class="['nav-link',{disabled: selected==tab}]">{{tab}}</a>
-          </li>
-          <li v-if="isLogged" class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              id="navbarDropdownMenuLink-4"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              <i class="fas fa-user"></i> Maria M
-            </a>
-            <div
-              class="dropdown-menu dropdown-menu-right dropdown-info"
-              aria-labelledby="navbarDropdownMenuLink-4"
-            >
-              <a class="dropdown-item" href="#" v-on:click="selected='MinhaConta'">Minha conta</a>
-              <a class="dropdown-item" href="#" v-on:click='logout()'>Sair</a>
-            </div>
-          </li>
-          <li v-else class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              id="navbarDropdownMenuLink-4"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >Sign in</a>
-            <div
-              class="dropdown-menu dropdown-menu-right dropdown-info"
-              aria-labelledby="navbarDropdownMenuLink-4"
-            >
-              <!-- Material form login -->
-              <div class="card">
-                <h5 class="card-header info-color white-text text-center py-4">
-                  <strong>Sign in</strong>
-                </h5>
-
-                <!--Card content-->
-                <div class="card-body px-lg-5 pt-0">
-                  <!-- Form -->
-                  <form class="text-center" style="color: #757575;">
-                    <br class="py-2">
-                    <input
-                      type="email"
-                      id="defaultLoginFormEmail"
-                      class="form-control mb-4"
-                      placeholder="E-mail"
-                      v-model="email"
-                    >
-
-                    <!-- Password -->
-                    <input
-                      type="password"
-                      id="defaultLoginFormPassword"
-                      class="form-control mb-4"
-                      placeholder="Password"
-                      v-model="password"
-                    >
-
-                    <div class="d-flex justify-content-around">
-                      <div>
-                        <!-- Remember me -->
-                        <div class="form-check">
-                          <input
-                            type="checkbox"
-                            class="form-check-input"
-                            id="materialLoginFormRemember"
-                          >
-                          <label
-                            class="form-check-label"
-                            for="materialLoginFormRemember"
-                          >Remember me</label>
-                        </div>
-                      </div>
-                      <div>
-                        <!-- Forgot password -->
-                        <a href>Forgot password?</a>
-                      </div>
-                    </div>
-
-                    <!-- Sign in button -->
-                    <button
-                      class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0"
-                      type="submit" style="border-radius: 2rem" v-on:click="loginIn()"
-                    >Sign in</button>
-
-                    <!-- Register -->
-                    <p>
-                      Not a member?
-                      <a href>Register</a>
-                    </p>
-                  </form>
-                  <!-- Form -->
-                </div>
-              </div>
-              <!-- Material form login -->
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <component :is="selected" :isLogged="isLogged"></component>
-    <footer class="page-footer font-small unique-color-dark py-1">
-      <!-- Footer Links -->
-      <div class="container text-center text-md-left mt-5">
-        <!-- Grid row -->
-        <div class="row mt-3">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase font-weight-bold">GreenHouse</h6>
-            <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <p>
-              <a href="#!">Home</a>
-            </p>
-            <p>
-              <a href="#!">About</a>
-            </p>
-            <p>
-              <a href="#!">Features</a>
-            </p>
-            <p>
-              <a href="#!">Pricing</a>
-            </p>
-            <p>
-              <a href="#!">Contact us</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <p>
-              <a href="#!">Blog</a>
-            </p>
-            <p>
-              <a href="#!">Search</a>
-            </p>
-            <p>
-              <a href="#!">T & Cs</a>
-            </p>
-            <p>
-              <a href="#!">Privacy</a>
-            </p>
-            <p>
-              <a href="#!">Community</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <p class="font-small">
-              <i class="mr-auto">Get our newsletter</i>
-            </p>
-            <form class="input-group">
-              <input
-                type="text"
-                class="form-control form-control-sm"
-                placeholder="Your email"
-                aria-label="Your email"
-                aria-describedby="basic-addon2"
+          <div  v-for="tab in tabs">
+            <div v-if="isLogged && (tab == 'Serviço' || tab=='Produtos')">
+              <li 
+              v-if="tab =='Serviço'"
+              :key='ServicoLogado'
+              @click="selected='ServicoLogado'"
+              :class="['nav-item',{active: selected == 'ServicoLogado'}]"
               >
-              <div class="input-group-append">
-                <button class="btn btn-sm btn-outline-white my-0" type="button">Subscribe</button>
-              </div>
-            </form>
-            <br>
-            <!--Facebook-->
-            <a class="fb-ic mr-3" role="button">
-              <i class="fab fa-lg fa-facebook-f"></i>
-            </a>
-            <!--Twitter-->
-            <a class="tw-ic mr-3" role="button">
-              <i class="fab fa-lg fa-twitter"></i>
-            </a>
-            <!--Google +-->
-            <a class="gplus-ic mr-3" role="button">
-              <i class="fab fa-lg fa-google-plus-g"></i>
-            </a>
-            <!--Instagram-->
-            <a class="ins-ic mr-3" role="button">
-              <i class="fab fa-lg fa-instagram"></i>
-            </a>
-            <!--Youtube-->
-            <a class="yt-ic mr-3" role="button">
-              <i class="fab fa-lg fa-youtube"></i>
-            </a>
-          </div>
-          <!-- Grid column -->
+
+              <a :class="['nav-link',{disabled: selected=='ServicoLogado'}]">{{tab}}</a>
+            </li>
+
+            <li 
+            v-else-if="tab =='Produtos'"
+            :key='ProdutoLogado'
+            @click="selected='ProdutoLogado'"
+            :class="['nav-item',{active: selected == 'ProdutoLogado'}]"
+            >
+
+            <a :class="['nav-link',{disabled: selected=='ProdutoLogado'}]">{{tab}}</a>
+          </li>
         </div>
-        <!-- Grid row -->
-      </div>
-    </footer>
+        <div v-else-if="!isLogged && (tab == 'Serviço' || tab=='Produtos')">
+          <li 
+          v-if="tab=='Serviço'"
+          :key='ServicoNLogado'
+          @click="selected='ServicoNLogado'"
+          :class="['nav-item',{active: selected == 'ServicoNLogado'}]"
+          >
+          <a :class="['nav-link',{disabled: selected=='ServicoNLogado'}]">{{tab}}</a>
+        </li>
+        <li 
+        v-if="tab=='Produtos'"
+        :key='ProdutoNLogado'
+        @click="selected='ProdutoNLogado'"
+        :class="['nav-item',{active: selected == 'ProdutoNLogado'}]"
+        >
+        <a :class="['nav-link',{disabled: selected=='ProdutoNLogado'}]">{{tab}}</a>
+      </li>
+
+    </div>
+    <div v-else>
+      <li 
+      :key="tab"
+      @click="selected=tab"
+      :class="['nav-item',{active: selected == tab}]"
+      >
+      <a :class="['nav-link',{disabled: selected==tab}]">{{tab}}</a>
+    </li>
   </div>
+
+
+
+</div>
+
+
+<li v-if="isLogged" class="nav-item dropdown">
+  <a
+  class="nav-link dropdown-toggle"
+  id="navbarDropdownMenuLink-4"
+  data-toggle="dropdown"
+  aria-haspopup="true"
+  aria-expanded="false"
+  >
+  <i class="fas fa-user"></i> Maria M
+</a>
+<div
+class="dropdown-menu dropdown-menu-right dropdown-info"
+aria-labelledby="navbarDropdownMenuLink-4"
+>
+<a class="dropdown-item" href="#" v-on:click="selected='MinhaConta'">Minha conta</a>
+<a class="dropdown-item" href="#" v-on:click='logout()'>Sair</a>
+</div>
+</li>
+<li v-else class="nav-item dropdown">
+  <a
+  class="nav-link dropdown-toggle"
+  id="navbarDropdownMenuLink-4"
+  data-toggle="dropdown"
+  aria-haspopup="true"
+  aria-expanded="false"
+  >Sign in</a>
+  <div
+  class="dropdown-menu dropdown-menu-right dropdown-info"
+  aria-labelledby="navbarDropdownMenuLink-4"
+  >
+  <!-- Material form login -->
+  <div class="card">
+    <h5 class="card-header info-color white-text text-center py-4">
+      <strong>Sign in</strong>
+    </h5>
+
+    <!--Card content-->
+    <div class="card-body px-lg-5 pt-0">
+      <!-- Form -->
+      <form class="text-center" style="color: #757575;">
+        <br class="py-2">
+        <input
+        type="email"
+        id="defaultLoginFormEmail"
+        class="form-control mb-4"
+        placeholder="E-mail"
+        v-model="email"
+        >
+
+        <!-- Password -->
+        <input
+        type="password"
+        id="defaultLoginFormPassword"
+        class="form-control mb-4"
+        placeholder="Password"
+        v-model="password"
+        >
+
+        <div class="d-flex justify-content-around">
+          <div>
+            <!-- Remember me -->
+            <div class="form-check">
+              <input
+              type="checkbox"
+              class="form-check-input"
+              id="materialLoginFormRemember"
+              >
+              <label
+              class="form-check-label"
+              for="materialLoginFormRemember"
+              >Remember me</label>
+            </div>
+          </div>
+          <div>
+            <!-- Forgot password -->
+            <a href>Forgot password?</a>
+          </div>
+        </div>
+
+        <!-- Sign in button -->
+        <button
+        class="btn btn-outline-info btn-rounded btn-block my-4 waves-effect z-depth-0"
+        type="submit" style="border-radius: 2rem" v-on:click="loginIn()"
+        >Sign in</button>
+
+        <!-- Register -->
+        <p>
+          Not a member?
+          <a href>Register</a>
+        </p>
+      </form>
+      <!-- Form -->
+    </div>
+  </div>
+  <!-- Material form login -->
+</div>
+</li>
+</ul>
+</div>
+</nav>
+<component :is="selected" :isLogged="isLogged"></component>
+<footer class="page-footer font-small unique-color-dark py-1">
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+    <!-- Grid row -->
+    <div class="row mt-3">
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold">GreenHouse</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+        <p>
+          <a href="#!">Home</a>
+        </p>
+        <p>
+          <a href="#!">About</a>
+        </p>
+        <p>
+          <a href="#!">Features</a>
+        </p>
+        <p>
+          <a href="#!">Pricing</a>
+        </p>
+        <p>
+          <a href="#!">Contact us</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+        <!-- Links -->
+        <p>
+          <a href="#!">Blog</a>
+        </p>
+        <p>
+          <a href="#!">Search</a>
+        </p>
+        <p>
+          <a href="#!">T & Cs</a>
+        </p>
+        <p>
+          <a href="#!">Privacy</a>
+        </p>
+        <p>
+          <a href="#!">Community</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        <p class="font-small">
+          <i class="mr-auto">Get our newsletter</i>
+        </p>
+        <form class="input-group">
+          <input
+          type="text"
+          class="form-control form-control-sm"
+          placeholder="Your email"
+          aria-label="Your email"
+          aria-describedby="basic-addon2"
+          >
+          <div class="input-group-append">
+            <button class="btn btn-sm btn-outline-white my-0" type="button">Subscribe</button>
+          </div>
+        </form>
+        <br>
+        <!--Facebook-->
+        <a class="fb-ic mr-3" role="button">
+          <i class="fab fa-lg fa-facebook-f"></i>
+        </a>
+        <!--Twitter-->
+        <a class="tw-ic mr-3" role="button">
+          <i class="fab fa-lg fa-twitter"></i>
+        </a>
+        <!--Google +-->
+        <a class="gplus-ic mr-3" role="button">
+          <i class="fab fa-lg fa-google-plus-g"></i>
+        </a>
+        <!--Instagram-->
+        <a class="ins-ic mr-3" role="button">
+          <i class="fab fa-lg fa-instagram"></i>
+        </a>
+        <!--Youtube-->
+        <a class="yt-ic mr-3" role="button">
+          <i class="fab fa-lg fa-youtube"></i>
+        </a>
+      </div>
+      <!-- Grid column -->
+    </div>
+    <!-- Grid row -->
+  </div>
+</footer>
+</div>
 </template>
 
 <script>
-import Inicio from "./components/Inicio.vue";
-import Sobre from "./components/Sobre.vue";
-import MinhaConta from "./components/MinhaConta.vue"
-
+  import Inicio from "./components/Inicio.vue";
+  import Sobre from "./components/Sobre.vue";
+  import MinhaConta from "./components/MinhaConta.vue"
+  import ServicoNLogado from "./components/ServicoNLogado.vue"
+  import ServicoLogado from "./components/ServicoLogado.vue"
+  import ProdutoNLogado from "./components/ProdutoNLogado.vue"
+  import ProdutoLogado from "./components/ProdutoLogado.vue"
 // class User{
 //   constructor(email, password, name){
 //     this.email = email;
@@ -230,11 +281,15 @@ export default {
   components: {
     Inicio,
     Sobre,
-    MinhaConta
+    MinhaConta, 
+    ServicoNLogado,
+    ServicoLogado,
+    ProdutoNLogado,
+    ProdutoLogado
   },
   data: () => {
     return {
-      tabs: ["Inicio", "Sobre"],
+      tabs: ["Inicio", "Sobre","Produtos", "Serviço"],
       selected: "Inicio",
       isLogged: false,
       email: "",
@@ -257,8 +312,8 @@ export default {
 </script>
 
 <style scoped>
-#app {
+  #app {
 
-}
+  }
 
 </style>
---
+++ greenhouse/src/components/ProdutoNLogado.vue
@@ -0,0 +1,206 @@
+<template>
+	<main>
+        <div class="row py-4" style="background-color:#63CF67">
+            <div class="container col-lg-4 ml-auto">
+                <img src="assets/house.png" alt="house" width="300px">
+            </div>
+            <div class="container col-lg-8 mr-auto">
+                <img SRC="assets/our_products.png" alt="Produtos" height="100px">
+                <p class="white-text" style="font-size: 60px;font-weight: bold;font-family: Lato">
+                    <b>
+                        Sustentabilidade <br>
+                        é nossa prioridade
+                    </b>
+                </p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <h1 style="text-align: center">
+                    Composteira
+                </h1>
+                <p class="font-small text-center">
+                    Gag ipsum dolar sit amet close enough i lied anonymous herp derp. Aww Yeah 9000 weekend joke
+                    hammer
+                    like
+                    a boss avenger games varius puking rainbows. Amet sweet jezus scumbag unlock cuteness overload
+                    pizza
+                    dead
+                    no asians. Russia fap fap fap le friend german dolar le me motherfucker fap fap brother fuck
+                    yeah. I
+                    Know That Feel y u no 140% le
+                    girlfriend dummies geek morbi cereal guy le derp wodka. Oh Stop It, You derpina food impossibru
+                    if
+                    you know what i mean grey
+                    so close genius stoned collection. True love gasp feel like a sir read female meme high school i
+                    see
+                    what
+                    you did there search left. Mom male jackie chan on back good guy mother luke. Homework oh god
+                    why
+                    fuck that shit essay keyboard
+                    all the things troll pokeman twitter tank michelle. Hac ipsum iron man hitler nother to do here
+                    phone sit on
+                    donut why. Facepalm laptop basic math the avengers sex monday.
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3">
+                <img src="assets/compost.jpg" alt="logo" class="ml-auto" height="300em">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-3">
+                <img src="assets/HomeBiogas.jpg" alt="logo" class="ml-auto" height="300em">
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <h1 style="text-align: center">
+                    HomeBiogas
+                </h1>
+                <p class="font-small" style="text-align:center">
+                    Gag ipsum dolar sit amet close enough i lied anonymous herp derp. Aww Yeah 9000 weekend joke
+                    hammer
+                    like
+                    a boss avenger games varius puking rainbows. Amet sweet jezus scumbag unlock cuteness overload
+                    pizza
+                    dead
+                    no asians. Russia fap fap fap le friend german dolar le me motherfucker fap fap brother fuck
+                    yeah. I
+                    Know That Feel y u no 140% le
+                    girlfriend dummies geek morbi cereal guy le derp wodka. Oh Stop It, You derpina food impossibru
+                    if
+                    you know what i mean grey
+                    so close genius stoned collection. True love gasp feel like a sir read female meme high school i
+                    see
+                    what
+                    you did there search left. Mom male jackie chan on back good guy mother luke. Homework oh god
+                    why
+                    fuck that shit essay keyboard
+                    all the things troll pokeman twitter tank michelle. Hac ipsum iron man hitler nother to do here
+                    phone sit on
+                    donut why. Facepalm laptop basic math the avengers sex monday.
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-3"></div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <h1 style="text-align: center">
+                    Decomposteira
+                </h1>
+                <p class="font-small" style="text-align:center">
+                    Gag ipsum dolar sit amet close enough i lied anonymous herp derp. Aww Yeah 9000 weekend joke
+                    hammer
+                    like
+                    a boss avenger games varius puking rainbows. Amet sweet jezus scumbag unlock cuteness overload
+                    pizza
+                    dead
+                    no asians. Russia fap fap fap le friend german dolar le me motherfucker fap fap brother fuck
+                    yeah. I
+                    Know That Feel y u no 140% le
+                    girlfriend dummies geek morbi cereal guy le derp wodka. Oh Stop It, You derpina food impossibru
+                    if
+                    you know what i mean grey
+                    so close genius stoned collection. True love gasp feel like a sir read female meme high school i
+                    see
+                    what
+                    you did there search left. Mom male jackie chan on back good guy mother luke. Homework oh god
+                    why
+                    fuck that shit essay keyboard
+                    all the things troll pokeman twitter tank michelle. Hac ipsum iron man hitler nother to do here
+                    phone sit on
+                    donut why. Facepalm laptop basic math the avengers sex monday.
+                </p>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3">
+                <img src="assets/decomposer.jpg" alt="logo" class="ml-auto" height="300em">
+            </div>
+        </div>
+
+        <h3 class="text-center">
+            Mais Produtos
+        </h3>
+
+        <div id="carousel-example-multi" class="carousel slide carousel-multi-item v-2" data-ride="carousel" style="height: 500px">
+
+            <div class="controls-top">
+                <a class="btn-floating" href="#carousel-example-multi" data-slide="prev"><i
+                        class="fas fa-chevron-left"></i></a>
+                <a class="btn-floating" href="#carousel-example-multi" data-slide="next"><i
+                        class="fas fa-chevron-right"></i></a>
+            </div>
+            <div class="carousel-inner v-2" style="padding-left:33%" role="listbox">
+                <div class="carousel-item active">
+                    <div class="col-md-6">
+                        <div class="card mb-2">
+                            <img class="card-img-top" src="assets/Produtos/home_biogas_1.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title font-weight-bold">Card title</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the
+                                    card's content.</p>
+                                <a class="btn btn-primary btn-md btn-rounded">Button</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-6">
+                        <div class="card mb-2">
+                            <img class="card-img-top" src="assets/Produtos/home_biogas_2.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title font-weight-bold">Card title</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the
+                                    card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-6">
+                        <div class="card mb-2">
+                            <img class="card-img-top" src="assets/Produtos/subpod.png" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title font-weight-bold">Card title</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the
+                                    card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-6">
+                        <div class="card mb-2">
+                            <img class="card-img-top" src="assets/Produtos/eletro.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title font-weight-bold">Card title</h4>
+                                <p class="card-text">Some quick example text to build on the card title and make up the
+                                    bulk of the
+                                    card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="py-4 row" style="background-color:#6DE16A; padding-left: 15%">
+            <div class="col-lg-5">
+                <h3 class="white-text">
+                    Pronto?
+                </h3>
+                <h1 class="white-text">
+                    Comece agora mesmo!
+                </h1>
+            </div>
+            <div class="col-lg-5">
+                <div class="input-group-append" style="padding-top:5%">
+                    <button class="btn btn-md btn-dark" type="button">Sign up</button>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
