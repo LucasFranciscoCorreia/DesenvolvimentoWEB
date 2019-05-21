340998ae499804e0ba7bc752e407a5067fb79e79
lucas <lfpgc96@hotmail.com>
2019-05-20 21:26:58 -0300

    Inicio de responsividade na pagina Inicio

--- greenhouse/src/App.vue
+++ greenhouse/src/App.vue
@@ -3,35 +3,138 @@
     <nav class="navbar navbar-expand-lg navbar-light mr-auto">
       <div class="collapse navbar-collapse ml-auto" id="navbarNav">
         <ul class="navbar-nav ml-auto">
-          <li v-for="tab in tabs" :key="tab" @click="selected=tab" :class="['nav-item',{active: selected == tab}]">
+          <li
+            v-for="tab in tabs"
+            :key="tab"
+            @click="selected=tab"
+            :class="['nav-item',{active: selected == tab}]"
+          >
             <a class="nav-link">{{tab}}</a>
           </li>
         </ul>
       </div>
     </nav>
-    <component :is="selected" class="tab"></component>
+    <component :is="selected"></component>
+    <footer class="page-footer font-small unique-color-dark py-1">
+      <!-- Footer Links -->
+      <div class="container text-center text-md-left mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase font-weight-bold">GreenHouse</h6>
+            <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <p>
+              <a href="#!">Home</a>
+            </p>
+            <p>
+              <a href="#!">About</a>
+            </p>
+            <p>
+              <a href="#!">Features</a>
+            </p>
+            <p>
+              <a href="#!">Pricing</a>
+            </p>
+            <p>
+              <a href="#!">Contact us</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <p>
+              <a href="#!">Blog</a>
+            </p>
+            <p>
+              <a href="#!">Search</a>
+            </p>
+            <p>
+              <a href="#!">T & Cs</a>
+            </p>
+            <p>
+              <a href="#!">Privacy</a>
+            </p>
+            <p>
+              <a href="#!">Community</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <p class="font-small">
+              <i class="mr-auto">Get our newsletter</i>
+            </p>
+            <form class="input-group">
+              <input
+                type="text"
+                class="form-control form-control-sm"
+                placeholder="Your email"
+                aria-label="Your email"
+                aria-describedby="basic-addon2"
+              >
+              <div class="input-group-append">
+                <button class="btn btn-sm btn-outline-white my-0" type="button">Subscribe</button>
+              </div>
+            </form>
+            <br>
+            <!--Facebook-->
+            <a class="fb-ic mr-3" role="button">
+              <i class="fab fa-lg fa-facebook-f"></i>
+            </a>
+            <!--Twitter-->
+            <a class="tw-ic mr-3" role="button">
+              <i class="fab fa-lg fa-twitter"></i>
+            </a>
+            <!--Google +-->
+            <a class="gplus-ic mr-3" role="button">
+              <i class="fab fa-lg fa-google-plus-g"></i>
+            </a>
+            <!--Instagram-->
+            <a class="ins-ic mr-3" role="button">
+              <i class="fab fa-lg fa-instagram"></i>
+            </a>
+            <!--Youtube-->
+            <a class="yt-ic mr-3" role="button">
+              <i class="fab fa-lg fa-youtube"></i>
+            </a>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import Inicio from "./components/Inicio.vue"
-import Sobre from "./components/Sobre.vue"
+import Inicio from "./components/Inicio.vue";
+import Sobre from "./components/Sobre.vue";
 export default {
   name: "app",
   components: {
-    Inicio,  Sobre
+    Inicio,
+    Sobre,
   },
   data: () => {
     return {
       tabs: ["Inicio", "Sobre"],
       selected: "Inicio"
     };
-  },
+  }
 };
 </script>
 
 <style>
 #app {
-  
 }
 </style>
--- greenhouse/src/components/Inicio.vue
+++ greenhouse/src/components/Inicio.vue
@@ -1,49 +1,38 @@
 <template>
   <main>
     <div id="banner_home" style="background-color: #0EBE57; color: white;">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-6" style="margin:auto;">
-            <div class="row">
-              <div class="col-sm-6">
-                <h1 style="text-align: right; ">
-                  <strong>Economize</strong>
-                </h1>
-              </div>
-              <div class="col-sm-6">
-                <h3 style="text-align: left;">dinheiro enquanto</h3>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm-6">
-                <h1 style="text-align: right;">
-                  <strong>Salva</strong>
-                </h1>
-              </div>
-              <div class="col-sm-6">
-                <h3 style="text-align: left;">o meio ambiente</h3>
-              </div>
-            </div>
-            <div class="row">
-              <br>
-              <br>
-              <p style="margin:auto;">
-                <strong>Se inscreva e faça parte da comunidade</strong>
-              </p>
-            </div>
-            <div class="row">
-              <button type="button" class="btn btn-dark" style="margin:auto;">Sign up</button>
-            </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-6 col-lg-6" style="margin:auto;">
+          <div class="row">
+            <p class="centered start">
+              <span class="focus">Economize</span> dinheiro enquanto
+            </p>
           </div>
-          <div class="col-sm-6">
-            <img src="assets/banner.png" height="428">
+          <div class="row">
+            <p class="centered start">
+              <span class="focus">Salva</span> o meio ambiente
+            </p>
           </div>
+          <div class="row">
+            <br>
+            <br>
+            <p style="margin:auto;">
+              <strong>Se inscreva e faça parte da comunidade</strong>
+            </p>
+          </div>
+          <div class="row">
+            <button type="button" class="btn btn-dark" style="margin:auto;">Sign up</button>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <img src="assets/banner.png" height="428">
         </div>
       </div>
     </div>
-    <div class="container">
+    <div>
       <div class="row">
-        <div class="col-sm-7">
+        <div class="col-sm-1 col-md-1 col-lg-1"></div>
+        <div class="col-sm-6 col-md-6 col-lg-6">
           <br>
           <br>
           <h1>
@@ -59,15 +48,15 @@
             consequat arcu. Pellentesque feugiat molestie enim a aliquam.
           </p>
         </div>
-        <div class="col-sm-5">
+        <div class="col-sm-5 col-md-5 col-lg-5">
           <img src="assets/browser.png" height="428">
         </div>
       </div>
       <div class="row">
-        <div class="col-sm-5">
-          <img src="assets/app.png" height="428">
+        <div class="col-sm-5 col-md-5 col-lg-5">
+          <img src="assets/app.png" height="428em">
         </div>
-        <div class="col-sm-7">
+        <div class="col-sm-6 col-md-6 col-lg-6">
           <br>
           <br>
           <h3>
@@ -79,10 +68,12 @@
             consequat arcu. Pellentesque feugiat molestie enim a aliquam.
           </p>
         </div>
+        <div class="col-sm-1 col-md-1 col-lg-1"></div>
       </div>
 
       <div class="row">
-        <div class="col-sm-7">
+        <div class="col-sm-1 col-md-1 col-lg-1"></div>
+        <div class="col-sm-5 col-md-5 col-lg-5">
           <br>
           <br>
           <h3>
@@ -94,20 +85,20 @@
             consequat arcu. Pellentesque feugiat molestie enim a aliquam.
           </p>
         </div>
-        <div class="col-sm-5">
-          <img src="assets/trash.png" height="428">
+        <div class="col-sm-6 col-md-6 col-lg-6">
+          <img src="assets/trash.png" height="428em">
         </div>
       </div>
     </div>
 
-    <div id="banner_home" style="background-color: #0EBE57;color: white;">
+    <div id="banner_home" class="py-4" style="background-color: #0EBE57;color: white;">
       <div class="container">
         <div class="row" style="margin:auto;">
-          <div class="col-sm-6">
+          <div class="col-sm-6 col-md-6 col-lg-6">
             <h3>Pronto?</h3>
             <h2>Comece agora mesmo!</h2>
           </div>
-          <div class="col-sm-6" style="margin:auto;">
+          <div class="col-sm-6 col-md-6 col-lg-6" style="margin:auto;">
             <button type="button" class="btn btn-dark">Sign up</button>
           </div>
         </div>
@@ -118,9 +109,20 @@
 
 <script>
 export default {
-    name: "Inicio"
+  name: "Inicio"
 };
 </script>
 
 <style scoped>
+.start {
+  font-size: 30px;
+  margin: auto;
+}
+.start .focus {
+  font-size: 40px;
+}
+
+.centered {
+  text-align: center;
+}
 </style>
